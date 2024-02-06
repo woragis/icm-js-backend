7b6cf398deb0b5551b9eb0b5df58528d210ce348
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2024-02-06 14:56:25 -0300

    Migrate app to javascript

--- src/app.js
+++ src/app.js
@@ -1,27 +1,32 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-const app = express_1();
-// var app = (0, express_1.default)();
-// app.use(
-//   cors({
-//     origin: "http://localhost:3000",
-//     methods: "GET, POST, PUT, PATCH, DELETE",
-//     credentials: true,
-//     optionsSuccessStatus: 204,
-//   })
-// );
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// import logger from "./middlewares/logger";
-// app.use(logger);
-// import contribRoutes from "./routes/contrib";
-// import reportRoutes from "./routes/report";
-// import authRoutes from "./routes/auth";
-// app.use("/", contribRoutes);
-// app.use("/report", reportRoutes);
-// app.use("/auth", authRoutes);
-app.get("/", function (req, res) {
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET, POST, PUT, PATCH, DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const logger = require("./middlewares/logger");
+
+app.use(logger);
+
+const contribRoutes = require("./routes/contrib");
+const reportRoutes = require("./routes/report");
+const authRoutes = require("./routes/auth");
+
+app.use("/", contribRoutes);
+app.use("/report", reportRoutes);
+app.use("/auth", authRoutes);
+
+app.get("/", (req, res) => {
   res.send("hi");
 });
-exports.default = app;
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var app_1 = require("./app");
-var port = process.env.PORT || 3000;
-app_1.default.listen(port, function () {
-    console.log("Server running on port https://localhost:" + port);
+const app = require("./app");
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log("Server running on port https://localhost:" + port);
 });
