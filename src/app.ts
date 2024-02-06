2007629f3b8ac959564760aeec1ca0d9744f0ca4
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2024-02-06 13:56:08 -0300

    Improve type code and packages

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,28 @@
 import express, { Application, Request, Response } from "express";
+import cors from "cors";
 const app: Application = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET, POST, PUT, PATCH, DELETE",
+    credentials: true,
+    optionsSuccessStatus: 204,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+import logger from "./middlewares/logger";
+
+app.use(logger);
 
 import contribRoutes from "./routes/contrib";
-import reportRoutes from "./routes/contrib";
+import reportRoutes from "./routes/report";
+import authRoutes from "./routes/auth";
 
-app.use(contribRoutes);
-app.use(reportRoutes);
+app.use("/", contribRoutes);
+app.use("/report", reportRoutes);
+app.use("/auth", authRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("hi");
