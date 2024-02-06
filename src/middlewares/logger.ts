9e21650a2a57eff49db599b7ec27f4c2bed6545e
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2024-02-06 14:52:29 -0300

    Migrate middlewares to javascript

--- src/middlewares/logger.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-const logger = (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.method, req.url, req.body);
-
-  next();
-};
-
-export default logger;
