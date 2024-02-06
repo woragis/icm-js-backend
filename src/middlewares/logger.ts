09a49faa594b927d9e7e15796ba9ba71161b9384
Jezreel de Andrade <masteringthecode.woragis+github@gmail.com>
2024-02-06 13:54:51 -0300

    Create logger middleware

---
+++ src/middlewares/logger.ts
@@ -0,0 +1,9 @@
+import { Request, Response, NextFunction } from "express";
+
+const logger = (req: Request, res: Response, next: NextFunction) => {
+  console.log(req.method, req.url, req.body);
+
+  next();
+};
+
+export default logger;
